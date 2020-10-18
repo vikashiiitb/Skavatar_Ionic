417de3340fe0357514bdcaf164f3d1ded6f232fa
vikash kumar <vikash@vikashs-MacBook-Pro.local>
2020-10-19 00:37:08 +0530

    Changing the header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,46 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  navigate: any;  
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
+    this.sideMenu();  
     this.initializeApp();
   }
+
+  sideMenu() {  
+    this.navigate =   
+    [  
+        { 
+        title : 'App',
+        url   : '/apps',
+        icon  : 'apps' 
+        },
+      { 
+        title : 'Book',  
+        url   : '/book',  
+        icon  : 'book'  
+      },   
+      {  
+        title : 'Paint',  
+        url   : '/paint',  
+        icon  : 'brush'   
+      },  
+      {  
+        title : 'Contacts',  
+        url   : '/contacts',  
+        icon  : 'contacts'  
+      },   
+      {
+          title : 'Facebook',
+          url   : '/facebook.com',
+          icon  : 'logo-facebook'
+      },
+    ];  
+  }  
 
   initializeApp() {
     this.platform.ready().then(() => {
